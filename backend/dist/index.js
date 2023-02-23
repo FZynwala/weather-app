75ca08443c731aeda63f3713d2ffa010eca3e189
Zynwala <filip.zynwala@capgemini.com>
2023-02-23 16:55:02 +0100

    fix

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,11 +8,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import dotenv from 'dotenv';
-import express from 'express';
-import fetch from 'node-fetch';
-dotenv.config();
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
@@ -22,11 +27,12 @@ app.get('/place/autocomplete/json', (req, res) => __awaiter(void 0, void 0, void
     let params = `input=${'bon'}&key=${process.env.API_KEY}&language=en&radius=5000&rankby=distance`;
     let finalApiUrl = `${apiUrl}${encodeURI(params)}`;
     try {
-        const response = yield fetch(finalApiUrl);
+        const response = yield (0, node_fetch_1.default)(finalApiUrl);
         res.send(response.json());
     }
     catch (e) {
         console.log(e);
+        res.send(e);
     }
 }));
 app.listen(port, () => {
